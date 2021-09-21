dd5f0a48f88f9d96f21acdba6f898e84b244b6ae
Tengyyy <hansmartintomingas@gmail.com>
2021-09-21 21:16:11 +0300

    Work on volume button and slider

--- MediaPlayer/src/application/Controller.java
+++ MediaPlayer/src/application/Controller.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.ScaleTransition;
@@ -11,12 +12,16 @@ import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -25,6 +30,7 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.SVGPath;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -49,6 +55,12 @@ public class Controller implements Initializable{
 	@FXML
 	Pane playPane;
 	
+	@FXML
+	Slider volumeSlider;
+	
+	@FXML
+	FlowPane volumeSliderPane;
+	
 	private File file;
 	private Media media;
 	private MediaPlayer mediaPlayer;
@@ -58,18 +70,26 @@ public class Controller implements Initializable{
 	private DoubleProperty mediaViewHeight;
 	
 	Image maximize, minimize, volumeUp, volumeDown, volumeMute;
+	
+	double volumeValue;
 
 
 	private Image start;
 	
 	private File maximizeFile, minimizeFile, playFile, pauseFile, startFile, volumeUpFile, volumeDownFile, volumeMuteFile;
 	
-	final Timeline timeline = new Timeline();
+	Timeline fullscreenTimeline;
+	
+	Timeline  volume;
 	
 	boolean muted = false;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		volumeSliderPane.setClip(new Rectangle(60, 36));
+		
+		volumeSlider.setTranslateX(-60);
 		
 		file = new File("hey.mp4");
 		media = new Media(file.toURI().toString());
@@ -119,6 +139,30 @@ public class Controller implements Initializable{
 		volumeButton.setBackground(Background.EMPTY);
 		volumeIcon.setImage(volumeUp);
 		
+		volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {	
+				
+				mediaPlayer.setVolume(volumeSlider.getValue() / 100);
+				
+				if(volumeSlider.getValue() == 0) {
+					volumeIcon.setImage(volumeMute);
+					muted = true;
+				}
+				else if(volumeSlider.getValue() < 50) {
+					volumeIcon.setImage(volumeDown);
+					muted = false;
+				}
+				else {
+					volumeIcon.setImage(volumeUp);
+				}
+				
+			}
+			
+		});
+		
+		//volumeSlider.setVisible(false);
 		
 	}
 	
@@ -178,23 +222,77 @@ public class Controller implements Initializable{
 	}
 	
 	public void fullScreenEnter() {
-	     
-	     timeline.setCycleCount(2);
-	     timeline.setAutoReverse(true);
-	     timeline.getKeyFrames().add(new KeyFrame(Duration.millis(200),
+		
+		fullscreenTimeline = new Timeline();
+		
+	     fullscreenTimeline.setCycleCount(2);
+	     fullscreenTimeline.setAutoReverse(true);
+	     fullscreenTimeline.getKeyFrames().add(new KeyFrame(Duration.millis(200),
 	    		 new KeyValue(fullScreenIcon.scaleYProperty(), 1.3)));
-	     timeline.getKeyFrames().add(new KeyFrame(Duration.millis(200),
+	     fullscreenTimeline.getKeyFrames().add(new KeyFrame(Duration.millis(200),
 	    		 new KeyValue(fullScreenIcon.scaleXProperty(), 1.3)));
 	     
-	     timeline.play();
+	     fullscreenTimeline.play();
+	     
+	     fullscreenTimeline.setOnFinished((e) -> {
+	    	 fullscreenTimeline.stop();
+	    	 fullscreenTimeline.getKeyFrames().clear();
+	     });
 	}
 	
 	public void fullSreenExit() {
 		
-		timeline.stop();
+		fullscreenTimeline = new Timeline();
+		
+		fullscreenTimeline.stop();
+		
+		fullscreenTimeline.getKeyFrames().clear();
 		
 		fullScreenIcon.scaleXProperty().set(1);
 		fullScreenIcon.scaleYProperty().set(1);
+	}
+	
+	public void volumeSliderEnter() {
+	    // display volume slider 
+		
+		volume = new Timeline();
+		
+		volume.setCycleCount(1);
+		volume.setAutoReverse(false);
+		
+		volume.getKeyFrames().add(new KeyFrame(Duration.millis(50),
+				new KeyValue(volumeSlider.translateXProperty(), 0, Interpolator.EASE_OUT)));
+
+		
+	    volume.play();
+	    
+	    volume.setOnFinished((e) -> {
+	    	volume.stop();
+	    	volume.getKeyFrames().clear();
+	    });
+
+	}
+	
+	public void volumeSliderExit() {
+		// hide volume slider
+		
+		volume.stop();
+		
+		volume = new Timeline();
+		
+		volume.getKeyFrames().clear();
+		
+		volume.getKeyFrames().add(new KeyFrame(Duration.millis(100),
+				new KeyValue(volumeSlider.translateXProperty(), -60, Interpolator.EASE_OUT)));
+		
+	    volume.play();
+	    
+	    volume.setOnFinished((e) -> {
+	    	volume.stop();
+			volume.getKeyFrames().clear();
+
+	    });
+		
 	}
 	
 	public void mute() {
@@ -203,12 +301,18 @@ public class Controller implements Initializable{
 			
 			muted = true;
 			volumeIcon.setImage(volumeMute);
-			mediaPlayer.setMute(muted);
+			mediaPlayer.setVolume(0);
+			
+			volumeValue = volumeSlider.getValue();
+			
+			volumeSlider.setValue(0);
 		}
 		else {
 			muted = false;
 			volumeIcon.setImage(volumeUp);
-			mediaPlayer.setMute(muted);
+			mediaPlayer.setVolume(volumeValue);
+			
+			volumeSlider.setValue(volumeValue);
 		}
 		
 	}
